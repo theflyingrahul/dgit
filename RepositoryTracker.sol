468bca7ea170b48608b4a0ad30c02532892a6abb
Rahul Pon <theflyingrahul@gmail.com>
2022-12-14 14:35:32 +0530

    Roles appear functional

--- RepositoryTracker.sol
+++ RepositoryTracker.sol
@@ -12,61 +12,78 @@ contract RepositoryTracker is Ownable, AccessControl {
     bytes32 public constant DEV_ROLE = keccak256("DEV_ROLE");
     bytes32 public constant AUDIT_ROLE = keccak256("AUDIT_ROLE");
 
-    constructor(string memory bafyHash, string memory friendlyName) {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-
-        _ipfsAddress = bafyHash;
-        _friendlyName = friendlyName;
-    }
-
     modifier onlyDeveloper() {
         require(hasRole(DEV_ROLE, msg.sender), "Caller is not a developer!");
         _;
     }
 
     modifier onlyAuditor() {
-        require(hasRole(AUDIT_ROLE, msg.sender), "Caller is not an auditor!");
+        require(
+            hasRole(AUDIT_ROLE, msg.sender) || hasRole(DEV_ROLE, msg.sender),
+            "Caller is not an auditor!"
+        );
         _;
     }
 
-    function addDeveloper(address memory devAddress) external onlyOwner {
+    function addDeveloper(address devAddress) public onlyOwner {
         _grantRole(DEV_ROLE, devAddress);
     }
 
-    function addDevelopers(address[] memory devAddresses) external onlyOwner {
+    function addDevelopers(address[] memory devAddresses) public onlyOwner {
         for (uint256 i = 0; i < devAddresses.length; i++) {
             _grantRole(DEV_ROLE, devAddresses[i]);
         }
     }
 
-    function addAuditor(address memory auditAddress) external onlyOwner {
+    function addAuditor(address auditAddress) public onlyOwner {
         _grantRole(AUDIT_ROLE, auditAddress);
     }
 
-    function addAuditors(address[] memory auditAddresses) external onlyOwner {
+    function addAuditors(address[] memory auditAddresses) public onlyOwner {
         for (uint256 i = 0; i < auditAddresses.length; i++) {
             _grantRole(AUDIT_ROLE, auditAddresses[i]);
         }
     }
 
-    // End ownership stuff!
+    // End roles stuff!
 
-    function setIpfsAddress(string calldata bafyHash) public {
+    constructor(
+        string memory bafyHash,
+        string memory friendlyName,
+        address[] memory devAddresses,
+        address[] memory auditAddresses
+    ) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        addDevelopers(devAddresses);
+        addAuditors(auditAddresses);
+
         _ipfsAddress = bafyHash;
-    }
-
-    function setFriendlyName(string calldata friendlyName)
-        public
-        onlySuperOwner
-    {
         _friendlyName = friendlyName;
     }
 
-    function getIpfsAddress() external view returns (string memory) {
+    function setIpfsAddress(string calldata bafyHash) public onlyDeveloper {
+        _ipfsAddress = bafyHash;
+    }
+
+    function setFriendlyName(string calldata friendlyName) public onlyOwner {
+        _friendlyName = friendlyName;
+    }
+
+    function getIpfsAddress()
+        external
+        view
+        onlyAuditor
+        returns (string memory)
+    {
         return _ipfsAddress;
     }
 
-    function getFriendlyName() external view returns (string memory) {
+    function getFriendlyName()
+        external
+        view
+        onlyAuditor
+        returns (string memory)
+    {
         return _friendlyName;
     }
 }
